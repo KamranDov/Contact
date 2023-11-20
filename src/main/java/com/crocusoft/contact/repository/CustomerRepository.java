81567a9384b2e4e820fdf2ab8f336bc93f0bf85a
Kamran <axmedovkamran19@gmail.com>
2023-11-20 16:02:14 +0400

    added

--- src/main/java/com/crocusoft/contact/repository/CustomerRepository.java
+++ src/main/java/com/crocusoft/contact/repository/CustomerRepository.java
@@ -1,7 +1,6 @@
 package com.crocusoft.contact.repository;
 
 import com.crocusoft.contact.domain.Customer;
-import com.crocusoft.contact.dto.CustomerDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
